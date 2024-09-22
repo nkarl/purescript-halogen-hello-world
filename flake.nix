09b871b97af2227eecd25d5153e042dcb6df3f59
nkarl <21993921+nkarl@users.noreply.github.com>
2024-09-22 12:56:09 -0700

    Updated Nix flake to use a legacy nix formatter.

--- flake.nix
+++ flake.nix
@@ -8,37 +8,35 @@
     flake-compat.flake = false;
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    purescript-overlay,
-    ...
-  }: let
-    name = "halogen-hello-world";
+  outputs = { self, nixpkgs, purescript-overlay, ... }:
+    let
+      name = "halogen-hello-world";
 
-    supportedSystems = [
-      "x86_64-linux"
-    ];
+      supportedSystems = [
+        "x86_64-linux"
+      ];
 
-    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-  in {
-    devShell = forAllSystems (system: let
-      overlays = [purescript-overlay.overlays.default];
-      pkgs = import nixpkgs {inherit system overlays;};
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
-      pkgs.mkShell {
-        inherit name;
-        buildInputs =
-          [
-            pkgs.esbuild
-            pkgs.nodejs_20
-            pkgs.nixpkgs-fmt
-            pkgs.purs
-            pkgs.purs-tidy
-            pkgs.purs-backend-es
-            pkgs.purescript-language-server
-            pkgs.spago-unstable
-          ];
-      });
-  };
+    {
+      devShell = forAllSystems (system:
+        let
+          overlays = [ purescript-overlay.overlays.default ];
+          pkgs = import nixpkgs { inherit system overlays; };
+        in
+        pkgs.mkShell {
+          inherit name;
+          buildInputs =
+            [
+              pkgs.esbuild
+              pkgs.nodejs_20
+              pkgs.purs
+              pkgs.purs-tidy
+              pkgs.purs-backend-es
+              pkgs.purescript-language-server
+              pkgs.spago-unstable
+            ];
+        });
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    };
 }
