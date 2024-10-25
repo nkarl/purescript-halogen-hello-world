5c49e11153ec9ff6ac4bf9d24130234694c2dd87
nkarl <21993921+nkarl@users.noreply.github.com>
2024-10-24 19:55:39 -0700

    Added a simple Halogen example for using RouteDuplex hash/setting browser location box.

--- src/Main.purs
+++ src/Main.purs
@@ -13,8 +13,9 @@ import Halogen.VDom.Driver (runUI)
 --import Next.Patterns.ParentInputs.Child00 as ParentInputs.Child00
 --import Next.Patterns.ParentInputs.Child01 as ParentInputs.Child01
 --import Next.Patterns.ChildNotifies.Parent as ChildNotifies.Parent
-import Next.Patterns.ChildNotifies.Example as ChildNotifies.Example
+--import Next.Patterns.ChildNotifies.Example as ChildNotifies.Example
 --import Next.Patterns.ParentQueries.Parent as ParentQueries.Parent
+import Next.Routing.Duplex as Duplex
 
 {-
   TODO: Make an AppM monad component to wrap all components
@@ -33,5 +34,6 @@ main =
       --ParentInputs.Child00.component unit body
       --ParentInputs.Child01.component unit body
       --ChildNotifies.Parent.component unit body
-      ChildNotifies.Example.component unit body
+      --ChildNotifies.Example.component unit body
       --ParentQueries.Parent.component unit body
+      Duplex.component unit body
---
+++ src/Next/Routing/Duplex.purs
@@ -0,0 +1,42 @@
+module Next.Routing.Duplex where
+
+import Prelude
+
+import Effect.Class (class MonadEffect)
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Routing.Hash (setHash)
+
+
+type State  = String
+
+data Action = SetHash String
+
+component :: forall q i o m. MonadEffect m => H.Component q i o m
+component =
+  H.mkComponent
+  { initialState: initialState
+  , render
+  , eval: H.mkEval H.defaultEval
+    { handleAction = handleAction
+    }
+  }
+
+  where
+    initialState _ = ""
+
+    render :: State -> H.ComponentHTML Action () m
+    render _ = do
+      let
+          s = "hello world"
+      HH.div_
+        [ HH.button
+        [ HE.onClick \_ -> SetHash s ]
+          [ HH.text (show s) ]
+        ]
+
+    handleAction = case _ of
+      SetHash s -> do
+        H.liftEffect $ setHash ("/" <> s)
+        H.modify_ \_ -> s
