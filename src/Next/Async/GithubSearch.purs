39acc9bab03a0e36e1199b26e7c972d100d3ae08
nkarl <21993921+nkarl@users.noreply.github.com>
2024-11-29 12:08:31 -0800

    Added a problem statement for this example.

--- src/Next/Async/GithubSearch.purs
+++ src/Next/Async/GithubSearch.purs
@@ -6,6 +6,28 @@ import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 
+{--
+  NOTE: interface description.
+
+  This is a simple interface that is centered on the browser window, that has 2 parts:
+    1. a search bar for the user to type in any combination of strings to search
+    2. a square box under the search bar to display the result of the search in real time.
+
+  NOTE: other requirements
+
+  The search functionality must be able to request as the user type. Use the following
+  guideline for development:
+    1. implement search for complete keywords
+      - only triggers a request when the user hits the Enter key or the button 'Search'.
+    2. implement search on key strokes.
+      - do this only after #1.
+
+  NOTE: other considerations
+
+    - Github should have a request cap (mabye 5000/day), so consider a timeout before each
+      request.
+--}  
+
 component :: forall q i o m. H.Component q i o m
 component =
   H.mkComponent
